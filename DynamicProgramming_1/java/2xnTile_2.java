17acf60063f69fda712d202c505b955ff692a5a2
ryuat <jungohryu@gmail.com>
2018-07-27 16:14:55 +0900

    solving 2nxTile_2.java

---
+++ DynamicProgramming_1/java/2xnTile_2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/* 
+ 11727
+ 2×n 2 타일링 
+ */
+public class Main{
+	
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] d = new int[1001];
+		
+		d[1] = 1;
+		d[2] = 3;
+		for(int i=3;i<=1000;i++) {
+			d[i] = (d[i-1] + d[i-2] + d[i-2])%10007;
+		}
+		
+		System.out.println(d[n]);
+	}
+}
+
+

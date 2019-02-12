afea493c30aec1348593d5dc35134e1021600e4d
ryu <jungohryu@gmail.com>
2019-02-12 17:58:51 +0900

    ?

--- math_1/baseConv_1_re.cpp
+++ math_1/baseConv_1_re.cpp
@@ -1,26 +1,28 @@
-/*
-baekjoon 2745 진법변환 1 
-간단하게 변경
-*/
-
 #include <iostream>
-#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
 using namespace std;
-int main() {
-	int ans = 0;
-	string s;
-	int b;
-	cin >> s >> b;
 
-	for (int i = 0; i < s.size(); i++) {
-		if ('0' <= s[i] && s[i] <= '9') // 0~9 사이의 숫자일 경우 
-			s[i] = s[i] - '0'; // '0'의 아스키 값을 뺀다.
-		else
-			s[i] = s[i] - 'A' + 10; // 'A'~'Z'일 경우
+int main(void){
+    int num, b, r;
+    string ans="";
+    
+    scanf("%d %d", &num, &b);
 
-		ans = ans * b + s[i]; // b가 n-1번 곱해지도록
-	}
-	cout << ans << endl;
 
-	return 0;
+    while(num != 0){
+        r = num%b;
+        
+        if(r >= 10){
+            ans += r + 'A' - 10;
+        }else{
+            ans += r + '0';
+        }
+        num /= b;
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans;
 }
